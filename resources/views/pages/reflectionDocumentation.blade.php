aa5e6a8de172b1e441f52d5f3228ad45bcfb518b
www-data <www-data@s5138647.elf.ict.griffith.edu.au>
2019-09-27 23:09:52 +1000

    Added more to my reflectionDocumentation page.

--- resources/views/pages/reflectionDocumentation.blade.php
+++ resources/views/pages/reflectionDocumentation.blade.php
@@ -11,6 +11,13 @@
 
     <div class="postNowPosts">
         <strong><p>Peer Review Reflection Document:</p></strong>
-        <p></p>
+        <p>
+            Being able to discuss and demonstrate the work I have done with other students in the course was extremely beneficial for my learning process.
+            From peer reviewing I was able to meet students taking the course who I had previously not spoken to. One of the students who peer reviewed me
+            during the semester was majoring in multi-media design course. This was extremely interesting to me as their programming background knowledge
+            was limited due to not having to undertake certain programming courses like you have to do when majoring in IT or Computer Science. From reviewing
+            this student I was able to see a different perspective on how they underwent and completed tasks. As a result of the peer review experience I personally
+            found it better helping a student if they had a coding issue or a certain question than showing and explaining my code. 
+        </p>
     </div>
 @endsection('content')
