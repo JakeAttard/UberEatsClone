b9d44a404532761f026f9469d656e889c69b02c4
www-data <www-data@s5138647.elf.ict.griffith.edu.au>
2019-09-28 21:36:51 +1000

    Changes to the single purchase styles.

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -5,32 +5,6 @@
 @endsection('title')
 
 @section('content')
-    <!-- <div class="container-fluid w-75">
-        <div class="row justify-content-center">
-            <div class="col-lg-6">
-                @if ($order)
-                    <div class="card mb-3">
-                        <div class="card-header">{{ $order->product }} - {{ $order->price }}</div>
-                        <div class="card-body">
-                            <p class="card-text">
-                                {{ $order->user }} <br>
-                                {{ $order->address }} <br>
-                                {{ $order->time }}
-                            </p>
-                        </div>
-                    </div>
-                @else
-                    <div class="card mb-3">
-                        <div class="card-body">
-                            <p class="card-text">
-                            Invalid order id
-                            </p>
-                        </div>
-                    </div>
-                @endif  
-            </div>  
-        </div>
-    </div> -->
 
     <div class="container" style="border:1px solid #cecece; padding-top: 20px; margin-top: 20px; text-align: center;">
         <div class="col-xl" style="padding-top: 20px; padding-bottom: 20px;">
@@ -38,14 +12,16 @@
                 <div class="card" style="width: 18rem; display:inline-block; margin-right: 10px; margin-top: 20px;">
                     <img class="card-img-top" src="{{asset('images/mcdonalds.jpg')}}" alt="Card image cap">
                     <div class="card-body">
-                        <div class="card-header">{{ $order->product }} - {{ $order->price }}</div>
-                            <p class="card-text">Order Name :  {{ $order->user }}</p>
-                            <p class="card-text">Order Address : {{ $order->address }}</p>
-                            <p class="card-text">Order Date Time : {{$order ->time }}</p>
+                        <p class="card-text">Product Name: {{ $order->product}}</p>
+                        <p class="card-text">Order Price: {{ $order->price}}</p>
+                        <p class="card-text">Order Name :  {{ $order->user }}</p>
+                        <p class="card-text">Order Address : {{ $order->address }}</p>
+                        <p class="card-text">Order Date Time : {{$order ->time }}</p>
                     </div>
                 </div>
             @else
                     <p>Error no invalid order id. Please try again!</p>
             @endif
         </div>
+
 @endsection('content')
